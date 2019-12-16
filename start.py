2ecccd3595ebf1687bf7afa7f1944cdbc8f8844c
olel <olel@olel.de>
2019-12-16 11:27:25 +0100

    JS Libs added

--- start.py
+++ start.py
@@ -13,6 +13,7 @@ from flask import Flask
 from vinrec.views.index import app as index_view_blueprint
 from vinrec.views.upload import app as upload_view_blueprint
 from vinrec.views.recorder import app as recorder_view_blueprint
+from vinrec.views.download import app as download_view_blueprint
 
 # Creating Flask app
 app = Flask(__name__)
@@ -22,3 +23,4 @@ app.secret_key = "this must not be secret in this case"
 app.register_blueprint(index_view_blueprint)
 app.register_blueprint(upload_view_blueprint, url_prefix="/upload")
 app.register_blueprint(recorder_view_blueprint, url_prefix="/recorder")
+app.register_blueprint(download_view_blueprint, url_prefix="/download")
